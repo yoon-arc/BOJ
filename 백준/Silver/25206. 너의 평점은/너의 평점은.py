16debd65cf3db45c9caf6e5d995483ebdb8d160a
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-03-04 22:44:06 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,11 @@
+total = 0
+score = 0
+grade = {'A+':4.5, 'A0':4.0, 'B+':3.5,'B0':	3.0,'C+':2.5,'C0':2.0,'D+':1.5,'D0':1.0,'F':0.0}
+for _ in range(20):
+    a,b,c = input().split()
+    if c == 'P':
+        continue
+    b = float(b)
+    total += b
+    score += (b*grade[c])
+print(f'{score/total:.6f}')
