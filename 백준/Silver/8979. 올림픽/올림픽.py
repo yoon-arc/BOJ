2b47d1afaa037f85ce7f879a54d30bc06155b8b0
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-05-11 11:31:37 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,18 +1,23 @@
 #국가의 수, 등수 알고싶은 나라
 N, K = map(int, input().split())
 countries = []
-count = 1
 #입력
 for i in range(N):
     num, g,s,b = map(int, input().split())
     countries.append([num,g,s,b])
 #정렬
 countries.sort(key = lambda x : (-x[1],-x[2],-x[3]))
-#동점 처리
-for i in range(len(countries)):
-    if countries[i][0] == K:
-        if countries[i-1][1:] == countries[i][1:]:
-            count -= 1
-        print(count)
+#출력(동점 처리 포함)
+rank = 1
+pg, ps, pb = 0,0,0
+for idx, (n,g,s,b) in enumerate(countries):
+    if idx == 0:
+        prev_medals = (g, s, b)
+    #메달 조합이 바뀌었을 때만
+    else: 
+        if(g,s,b) != (pg,ps,pb):
+            rank = idx + 1
+            pg,ps,pb = g,s,b
+    if n == K:
+        print(rank)
         break
-    count += 1
