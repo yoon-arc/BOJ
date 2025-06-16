42b1e5878ee3e4cfc59053e2ae767790005e8213
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-06-16 23:37:42 +0900

    [Bronze III] Title: 주식 투자, Time: 1628 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/13416. 주식 투자/주식 투자.py
@@ -0,0 +1,10 @@
+T = int(input())
+for _ in range(T):
+    days = int(input())
+    total = 0
+    for d in range(days):
+        a,b,c = map(int, input().split())
+        if a<0 and b <0 and c <0 :
+            continue
+        total += max(a,b,c)
+    print(total)
