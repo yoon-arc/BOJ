ee1c3f9b171013235869488b07baf3101d7b1def
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-06-17 23:58:22 +0900

    [Bronze II] Title: 저항, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+colors = {'black':[0,1], 'brown':[1,10], 'red':[2,100], 'orange':[3,1000], 'yellow':[4,10000],
+         'green':[5,100000], 'blue':[6,1000000],'violet':[7,10000000], 'grey':[8,100000000],'white':[9,1000000000]}
+jhs = list(input().strip() for _ in range(3))
+print(int(str(colors[jhs[0]][0])+str(colors[jhs[1]][0])) * colors[jhs[2]][1])
