ce2070b600b4a8d7a3b756a7f2a1d4335e193408
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-09-23 23:05:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 164 ms, Memory: 42140 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,11 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-people = {} 
+people = {}
+
 for _ in range(N):
     name, log = input().split()
     if log == 'enter':
         people[name] = True
     else:  # 'leave'
         people.pop(name, None)
-
 for name in sorted(people.keys(), reverse=True):
     print(name)
