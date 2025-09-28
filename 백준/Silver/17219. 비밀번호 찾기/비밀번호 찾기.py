8b559d521b762b5702cef84527925bfb8edeadf5
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-09-28 10:11:05 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 216 ms, Memory: 48452 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,8 +1,10 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 pwds = {}
 for _ in range(N):
     s, p = input().split()
     pwds[s] = p
 for _ in range(M):
-    q = input()
+    q = input().rstrip()
     print(pwds[q])
