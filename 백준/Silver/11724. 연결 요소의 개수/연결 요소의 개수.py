646c12b5c9e9aa7d12b150b0b451e06092c6017f
yoonlix <108873618+yoon-arc@users.noreply.github.com>
2025-09-30 19:41:18 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 368 ms, Memory: 173800 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,33 @@
-import sys
-sys.setrecursionlimit(10000)
-input = sys.stdin.readline
-n, m = map(int, input().split())
-A = [[] for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
+N, M = map(int, input().split())
+graph = [ [] for _ in range(N + 1)]
+visited = [False for _ in range(N + 1)]
+times = 0
 
-def DFS(v):
-    visited[v] = True
-    for i in A[v]:
-        if not visited[i]:
-            DFS(i)
 
-count = 0
-
-for _ in range(m):
+for i in range(M):
     u, v = map(int, input().split())
-    A[u].append(v)
-    A[v].append(u)
-    
+    graph[u].append(v)
+    graph[v].append(u)
 
-for i in range(1,n+1):
-    if not visited[i]:
-        count += 1
-        DFS(i)
-# print(A)
-print(count)
+
+
+def dfs(start_node):
+    for i in graph[start_node]:
+        if visited[i] == False:
+            visited[i] = True
+            dfs(i)
+
+
+for i in range(1, N + 1):
+    if visited[i] == False:
+        times += 1
+        dfs(i)
+       
+        
+
+
+
+start_node = 1
+visited[start_node] = True
+dfs(start_node)
+print(times)
